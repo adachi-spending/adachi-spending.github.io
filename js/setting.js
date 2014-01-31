0fbb7c9769351fad7acc91801c31a2485bbf2743
jollyjoester <jollyjoester@gmail.com>
2014-02-01 01:53:45 +0900

    fix icon setting

--- js/setting.js
+++ js/setting.js
@@ -32,28 +32,28 @@ OpenSpending.Styles.Cofog = {
   '3-2': { icon: 'icons/family.svg', color: '#138EC0', bcolor: '#4E6D00' },
   '3-3': { icon: 'icons/family.svg', color: '#138EC0', bcolor: '#4E6D00' },
   '3-4': { icon: 'icons/family.svg', color: '#138EC0', bcolor: '#4E6D00' },
-  '4': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#D33673' },//産業経済費
-  '4-1': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#D33673' },
-  '4-2': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#D33673' },
-  '5': { icon: 'icons/human-resources.svg', color: '#138EC0', bcolor: '#2A3A03' },//環境衛生費
-  '5-1': { icon: 'icons/human-resources.svg', color: '#138EC0', bcolor: '#2A3A03' },
-  '6': { icon: 'icons/forest.svg', color: '#138EC0', bcolor: '#EC2406' },//土木費
-  '6-1': { icon: 'icons/forest.svg', color: '#138EC0', bcolor: '#EC2406' },
-  '6-2': { icon: 'icons/forest.svg', color: '#138EC0', bcolor: '#EC2406' },
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#138EC0', bcolor: '#938626' },//教育費
-  '7-1': { icon: 'icons/traffic-watersup.svg', color: '#138EC0', bcolor: '#938626' },
-  '8': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#C75746' },//公債費
-  '8-1': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#C75746' },
-  '8-2': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#C75746' },
-  '8-3': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#C75746' },
-  '8-4': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#C75746' },
-  '8-5': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#C75746' },
+  '4': { icon: 'icons/traffic-watersup.svg', color: '#138EC0', bcolor: '#D33673' },//産業経済費
+  '4-1': { icon: 'icons/traffic-watersup.svg', color: '#138EC0', bcolor: '#D33673' },
+  '4-2': { icon: 'icons/traffic-watersup.svg', color: '#138EC0', bcolor: '#D33673' },
+  '5': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#2A3A03' },//環境衛生費
+  '5-1': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#2A3A03' },
+  '5-2': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#2A3A03' },
+  '5-3': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#2A3A03' },
+  '6': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#EC2406' },//土木費
+  '6-1': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#EC2406' },
+  '6-2': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#EC2406' },
+  '6-3': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#EC2406' },
+  '6-4': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#EC2406' },
+  '7': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#938626' },//教育費
+  '7-1': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#938626' },
+  '7-2': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#938626' },
+  '7-3': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#938626' },
+  '7-4': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#938626' },
+  '7-5': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#938626' },
+  '7-6': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#938626' },
+  '8': { icon: 'icons/money.svg', color: '#138EC0', bcolor: '#C75746' },//公債費
   '9': { icon: 'icons/order-safety.svg', color: '#138EC0', bcolor: '#D33673' },//諸支出費
-  '9-1': { icon: 'icons/order-safety.svg', color: '#138EC0', bcolor: '#D33673' },
-  '10': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#790586' },//予備費
-  '10-1': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#790586' },
-
-  
+  '10': { icon: 'icons/financial-admin.svg', color: '#138EC0', bcolor: '#790586' },//予備費
 };
 
 
